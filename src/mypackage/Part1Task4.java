51b0a8d350a448486f9b74d61c6e398257d57996
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-01 11:31:02 +0300

    Part 1, Task 4

---
+++ src/mypackage/Part1Task4.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Part1Task4 {
+    public static void main(String[] args) {
+        System.out.println("Задание 4");
+        String name1 = "Vasia";
+        String name2 = "Vasia";
+        if (name1.equals(name2)) {
+            System.out.println(name1 + " и " + name2 + " - тезки");
+        } else {
+            System.out.println(name1 + " и " + name2 + " - не тезки");
+        };
+    }
+}
