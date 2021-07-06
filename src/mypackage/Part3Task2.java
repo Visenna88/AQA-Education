818e8b9b880cbc9699038c2e62ec1bc81a548a8b
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-06 09:50:13 +0300

    Task 2, Part 2 + Task 2 Part 3

---
+++ src/mypackage/Part3Task2.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part3Task2 {
+    public static void main(String[] args) {
+    List<Integer> arr2 = new ArrayList<Integer>();
+
+
+    }
+}
