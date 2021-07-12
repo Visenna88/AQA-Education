de06c0b1864da7aea5efaa21f9b52234b3b23c78
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-12 12:06:37 +0300

    Homework edits, Part1 Task4

--- src/mypackage/Part1Task4.java
+++ src/mypackage/Part1Task4.java
@@ -3,12 +3,14 @@ package mypackage;
 public class Part1Task4 {
     public static void main(String[] args) {
         System.out.println("Задание 4");
-        String name1 = "Vasia";
-        String name2 = "Vasia";
+        String name1 = " Vasia";
+        String name2 = "vasia";
+        name1 = name1.trim().toLowerCase();
+        name2 = name2.trim().toLowerCase();
         if (name1.equals(name2)) {
-            System.out.println(name1 + " и " + name2 + " - тезки");
+            System.out.println("Эти люди - тезки");
         } else {
-            System.out.println(name1 + " и " + name2 + " - не тезки");
+            System.out.println("Эти люди - не тезки");
         };
     }
 }
