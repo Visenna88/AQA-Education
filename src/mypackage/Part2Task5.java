163a15d445bfcc65c2b209de6fd7d1bc0d6ad48c
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-12 16:14:24 +0300

    Homework edits, Part 2, Task 5

--- src/mypackage/Part2Task5.java
+++ src/mypackage/Part2Task5.java
@@ -3,7 +3,7 @@ package mypackage;
 public class Part2Task5 {
     public static void main(String[] args) {
         System.out.println("Задание 5");
-        System.out.println("Первые 10 чисел последовательности: ");
+        System.out.println("Первые 10 чисел последовательности: вариант с while");
         int a5 = 0;
         int diff = -5;
         int n5 = 1;
@@ -11,6 +11,12 @@ public class Part2Task5 {
         while (1 <= n5 && n5 <= 10){
             int answ = (a5 + (n5 - 1)*diff);
             n5++;
+            System.out.print(answ + "; ");
+        }
+        System.out.println();
+        System.out.println("\nПервые 10 чисел последовательности: вариант с for");
+        for (n5 = 1; 1 <= n5 && n5 <= 10; n5++){
+            int answ = (a5 + (n5 - 1)*diff);
             System.out.print(answ + "; ");
         }
 
