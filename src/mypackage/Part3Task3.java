06d6431021daa234f5bbc74bcbb6f6648cbcef6c
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-13 17:43:06 +0300

    Part 3, task 3, edits

--- src/mypackage/Part3Task3.java
+++ src/mypackage/Part3Task3.java
@@ -6,16 +6,17 @@ import java.util.Arrays;
 public class Part3Task3 {
     public static void main(String[] args) {
         int[] arr2 = {7,-2,5,-1};
-        int min = arr2[0];
+        int max = arr2[arr2.length -1];
         Arrays.sort(arr2);
         System.out.println("Задание 3, вариант 1");
-        System.out.println("Наименьший элемент массива: " + arr2[0]);
+        System.out.println("Наибольший элемент массива: " + arr2[arr2.length -1]);
+
         System.out.println("\nЗадание 3, вариант 2 (for)");
         for (int i = 0; i <= arr2.length - 1; i++){
-            if (min >= arr2[i]){
-                min = arr2[i];
+            if (max <= arr2[i]){
+                max = arr2[i];
             }
         }
-        System.out.println("Наименьший элемент массива: " + min);
+        System.out.println("Наибольший элемент массива: " + max);
     }
 }
