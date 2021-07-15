d6fb6b26e23a6ad67acafb7cc91ff5b5c5edffc1
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-07-15 11:44:46 +0300

    Part 3, Task 6

---
+++ src/mypackage/Part3Task6.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+import java.util.Arrays;
+
+public class Part3Task6 {
+    public static void main(String[] args) {
+        int [] arr6 = new int[20];
+        for (int i = 0; i < arr6.length; i++) {
+            arr6[i] = (int) ((Math.random() * 1000) + 1);
+        }
+        System.out.println("Массив, заполненный случайными числами: " + Arrays.toString(arr6));
+    }
+}
