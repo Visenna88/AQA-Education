aa3b39ac00007f4d92b59d1dec6b3ded6e46ec83
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-08-13 12:40:40 +0300

    Part 4, Task 1 Dog

---
+++ src/mypackage/mainTestClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class mainTestClass {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.color = "red";
+        dog1.age = 2;
+
+        dog1.bark();
+        dog1.walk();
+    }
+
+}
