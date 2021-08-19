fe881a84b3ef24f87dcbc903b529ee4b415db607
Nadiia Sikarova <nadezhda.sirbu@gmail.com>
2021-08-19 12:49:58 +0300

    Bubble sorting + random array

--- src/mypackage/Part3Task6.java
+++ src/mypackage/Part3Task6.java
@@ -9,7 +9,21 @@ public class Part3Task6 {
             arr6[i] = (int) ((Math.random() * 1000) + 1);
         }
         System.out.println("Массив, заполненный случайными числами: " + Arrays.toString(arr6));
-        
+
+        boolean sorted = false;
+        int temp;
+        while(!sorted) { sorted = true;
+            for (int i = 0; i < arr6.length - 1; i++) {
+                    if (arr6[i] > arr6[i+1]) {
+                        temp = arr6[i];
+                        arr6[i] = arr6[i+1];
+                        arr6[i+1] = temp;
+                        sorted = false;
+                    }
+                }
+            }
+        System.out.println("Массив, отсоритрованный пузырьком: " + Arrays.toString(arr6));
+        }
 
     }
-}
+
